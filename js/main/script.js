5b5fe3fce8ea7e5b08e02930e372a4676ee791b2
nativelocation <bluesky6460@mail.com>
2019-03-02 13:17:26 -0500

    update

--- js/main/script.js
+++ js/main/script.js
@@ -1,20 +1,18 @@
 function addEventListeners() {
-	const component = document.querySelector('#hl_widget_e235001');
+	const component = document.querySelector('#hl_widget_e235002');
 
 	component.addEventListener('dragstart', onDragStart);
 	component.addEventListener('dragend', onDragEnd);
 
 	const form_builder = document.querySelector('#the-form-builder');
 	form_builder.addEventListener('drop', onDrop);
-	// form_builder.addEventListener('dragenter', onDragEnter);
-	// form_builder.addEventListener('dragleave', onDragLeave);
 	form_builder.addEventListener('dragover', onDragOver);
 }
 
 function onDragStart(event) {
 	console.log("dragStart", event.target.id);
-	// event.currentTarget.style.border = "dashed";
-	event.dataTransfer.setData("text", event.target.id);
+	event.dataTransfer.setData("id", event.target.id);
+	event.dataTransfer.setData("label", event.target.textContent);
 	event.effectAllowed = "copyMove";
 }
 
@@ -25,23 +23,116 @@ function onDragEnd(event) {
 }
 
 function onDrop(event) {
-	console.log("Drop");
+	console.log("Drop", event.target, event.currentTarget);
 	event.preventDefault();
-	var id = event.dataTransfer.getData("text");
-	// if (id == "src_copy" && ev.target.id == "dest_copy") {
-	console.log(id);
-	 var nodeCopy = document.getElementById(id).cloneNode(true);
-	 nodeCopy.id = id + "_copyId";
-	 nodeCopy.style.border = "none";
-	 nodeCopy.style.backgroundColor="#dddde8";
-	 event.target.appendChild(nodeCopy);
-	// }
+	var id = event.dataTransfer.getData("id");
+	var tgt = document.createElement("div");
+	tgt.className = "form-builder--item"
+	tgt.id = id + "_copyId";
+	var newLabel = document.createElement("label");
+	newLabel.textContent = event.dataTransfer.getData("label");
+	tgt.appendChild(newLabel);
+	var newInput = document.createElement("input");
+	newInput.className = "form-control";
+	newInput.type = "text";
+	newInput.placeholder = event.dataTransfer.getData("label");
+	tgt.appendChild(newInput);
+	event.target.appendChild(tgt);
 }
 
 function onDragOver(event) {
 	console.log("dragOver");
-	// event.currentTarget.style.background = "lightblue";
 	event.preventDefault();
+}
+
+function onLayoutChange(event) {
+	console.log('onLayoutChange', event.target.checked);
+	const form_builder = document.querySelector('#the-form-builder');
+	if (event.target.checked) {
+		form_builder.style.width = "auto";
+	} else {
+		form_builder.style.width = "500px";
+	}
+}
+
+function onBackgroundColorPickerInput(event) {
+	console.log('onBackgroundColorPickerInput', event.target.value);
+	const component = document.querySelector('.background.color-picker .the-color');
+	component.style.background = "#" + event.target.value;
+	const form_builder = document.querySelector('#the-form-builder');
+	form_builder.style.background = "#" + event.target.value;
+}
+
+function onFontColorPickerInput(event) {
+	console.log('onFontColorPickerInput', event.target.value);
+	const component = document.querySelector('.font.color-picker .the-color');
+	component.style.background = "#" + event.target.value;
+	const form_builder = document.querySelector('#the-form-builder');
+	form_builder.style.color = "#" + event.target.value;
+}
+
+function onBorderWidthInput(event) {
+	console.log('onBorderPicker', event.target.value);
+	const form_builder = document.querySelector('#the-form-builder');
+	form_builder.style.borderWidth = event.target.value + 'px';
+}
+
+function onBorderPickerUp(event) {
+	const component = document.querySelector('.border-picker .form-control');
+	component.value = parseInt(component.value) + 1;
+	const form_builder = document.querySelector('#the-form-builder');
+	form_builder.style.borderWidth = component.value + 'px';
+}
+
+function onBorderPickerDown(event) {
+	const component = document.querySelector('.border-picker .form-control');
+	if (parseInt(component.value) > 0) {
+		component.value = parseInt(component.value) - 1;
+	}
+	const form_builder = document.querySelector('#the-form-builder');
+	form_builder.style.borderWidth = component.value + 'px';
+}
+
+function onCornerRadiusInput(event) {
+	const form_builder = document.querySelector('#the-form-builder');
+	form_builder.style.borderRadius = event.target.value + 'px';
+}
+
+function onCornerRadiusUp(event) {
+	const component = document.querySelector('.corner-radius.pixel-count .form-control');
+	component.value = parseInt(component.value) + 1;
+	const form_builder = document.querySelector('#the-form-builder');
+	form_builder.style.borderRadius = component.value + 'px';
+}
+
+function onCornerRadiusDown(event) {
+	const component = document.querySelector('.corner-radius.pixel-count .form-control');
+	if (parseInt(component.value) > 0) {
+		component.value = parseInt(component.value) - 1;
+	}
+	const form_builder = document.querySelector('#the-form-builder');
+	form_builder.style.borderRadius = component.value + 'px';
+}
+
+function onWidthInput(event) {
+	const form_builder = document.querySelector('#the-form-builder');
+	form_builder.style.width = event.target.value + 'px';
+}
+
+function onWidthUp(event) {
+	const component = document.querySelector('.width.pixel-count .form-control');
+	component.value = parseInt(component.value) + 1;
+	const form_builder = document.querySelector('#the-form-builder');
+	form_builder.style.width = component.value + 'px';
+}
+
+function onWidthDown(event) {
+	const component = document.querySelector('.width.pixel-count .form-control');
+	if (parseInt(component.value) > 0) {
+		component.value = parseInt(component.value) - 1;
+	}
+	const form_builder = document.querySelector('#the-form-builder');
+	form_builder.style.width = component.value + 'px';
 }
 
 addEventListeners();
